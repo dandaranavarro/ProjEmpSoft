a214037fa115534312762acdc6d9592297cd3c40
dandaranavarro <dandara.navarro@ccc.ufcg.edu.br>
2015-11-18 15:56:25 -0300

    Adicionando as opções de login

--- src/my/projempsoft/TelaLogin.java
+++ src/my/projempsoft/TelaLogin.java
@@ -27,23 +27,18 @@ public class TelaLogin extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
         jTextField2 = new javax.swing.JTextField();
         jTextField3 = new javax.swing.JTextField();
         jTextField4 = new javax.swing.JTextField();
         jTextField5 = new javax.swing.JTextField();
         jButton2 = new javax.swing.JButton();
+        jRadioButton1 = new javax.swing.JRadioButton();
+        jRadioButton2 = new javax.swing.JRadioButton();
         jLabel4 = new javax.swing.JLabel();
-        filler1 = new javax.swing.Box.Filler(new java.awt.Dimension(0, 0), new java.awt.Dimension(0, 0), new java.awt.Dimension(0, 0));
-
-        jLabel1.setText("jLabel1");
-
-        jLabel3.setText("jLabel3");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        getContentPane().setLayout(null);
 
         jTextField1.setEditable(false);
         jTextField1.setBackground(java.awt.Color.darkGray);
@@ -57,7 +52,8 @@ public class TelaLogin extends javax.swing.JFrame {
                 jTextField1ActionPerformed(evt);
             }
         });
-        getContentPane().add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 180, 220, 70));
+        getContentPane().add(jTextField1);
+        jTextField1.setBounds(580, 180, 220, 70);
 
         jTextField2.setEditable(false);
         jTextField2.setBackground(new java.awt.Color(102, 102, 102));
@@ -73,7 +69,8 @@ public class TelaLogin extends javax.swing.JFrame {
                 jTextField2ActionPerformed(evt);
             }
         });
-        getContentPane().add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 370, 100, 40));
+        getContentPane().add(jTextField2);
+        jTextField2.setBounds(580, 370, 100, 40);
 
         jTextField3.setEditable(false);
         jTextField3.setBackground(new java.awt.Color(102, 102, 102));
@@ -84,17 +81,20 @@ public class TelaLogin extends javax.swing.JFrame {
         jTextField3.setBorder(null);
         jTextField3.setMargin(new java.awt.Insets(0, 0, 0, 0));
         jTextField3.setOpaque(false);
-        getContentPane().add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 280, 100, 40));
+        getContentPane().add(jTextField3);
+        jTextField3.setBounds(580, 280, 100, 40);
 
         jTextField4.setBackground(new java.awt.Color(204, 204, 204));
         jTextField4.setToolTipText("Digite sua senha");
         jTextField4.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        getContentPane().add(jTextField4, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 410, 310, 30));
+        getContentPane().add(jTextField4);
+        jTextField4.setBounds(580, 410, 310, 30);
 
         jTextField5.setBackground(new java.awt.Color(204, 204, 204));
         jTextField5.setToolTipText("Digite seu usuário");
         jTextField5.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        getContentPane().add(jTextField5, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 320, 310, 30));
+        getContentPane().add(jTextField5);
+        jTextField5.setBounds(580, 320, 310, 30);
 
         jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/my/projempsoft/images/abrir.png"))); // NOI18N
         jButton2.setBorderPainted(false);
@@ -109,13 +109,39 @@ public class TelaLogin extends javax.swing.JFrame {
                 jButton2ActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 460, 140, 50));
+        getContentPane().add(jButton2);
+        jButton2.setBounds(670, 520, 140, 50);
+
+        jRadioButton1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jRadioButton1.setForeground(new java.awt.Color(255, 255, 255));
+        jRadioButton1.setSelected(true);
+        jRadioButton1.setText("Vendedor");
+        jRadioButton1.setOpaque(false);
+        jRadioButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jRadioButton1);
+        jRadioButton1.setBounds(580, 480, 120, 30);
+
+        jRadioButton2.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jRadioButton2.setForeground(new java.awt.Color(255, 255, 255));
+        jRadioButton2.setText("Gerente");
+        jRadioButton2.setOpaque(false);
+        jRadioButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioButton2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jRadioButton2);
+        jRadioButton2.setBounds(580, 450, 120, 30);
 
         jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/my/projempsoft/images/background.png"))); // NOI18N
         jLabel4.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jLabel4.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
-        getContentPane().add(filler1, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 390, -1, -1));
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(0, 0, 1086, 774);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -137,6 +163,14 @@ public class TelaLogin extends javax.swing.JFrame {
         new CadastroCliente().setVisible(true);
         this.dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jRadioButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jRadioButton1ActionPerformed
+
+    private void jRadioButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jRadioButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -174,11 +208,10 @@ public class TelaLogin extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.Box.Filler filler1;
     private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JRadioButton jRadioButton1;
+    private javax.swing.JRadioButton jRadioButton2;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
